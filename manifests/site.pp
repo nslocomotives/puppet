41e9b8c476e070f7f3369373a00cac5e9532ba16
Nigel Smith <nslocomotives@hotmail.com>
2017-08-15 11:18:46 +0100

    adding regex for jenkins

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,3 +2,9 @@ node default {
     include cron-puppet
     include nslocomotives-sshd
 }
+
+node /^jenkins\d+\.local/ {
+    include cron-puppet
+    include nslocomotives-sshd
+    include apache
+} 
